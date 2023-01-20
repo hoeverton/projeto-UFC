4a24aad7da5675ed86c2fe005b9b7c9dea82db49
Hoéverton D. Ziwert <hoeverton.ziwert@gmail.com>
2023-01-20 08:58:17 -0300

    commit inicial

---
+++ UFC/luta.py
@@ -0,0 +1,92 @@
+from lutador import Lutador
+
+class Luta:
+
+    def __init__(self, desafiado, desafiante, rounds) -> None:
+        
+        self.__desafiado = desafiado
+        self.__desafiante = desafiante
+        self.__rounds = rounds
+        self.__aprovada = False
+
+    
+    
+    def marcarLuta(self, l1, l2):
+
+        l1 = l1(self.Lutador.get_cateoria())
+        l2 = l2(self.Lutador.get_cateoria())
+
+        if l1 == l2:
+            self.aprovado = True
+            print('Luta APROVADA')
+
+        else:
+            print('Luta inválida os participantes dever ser da mesma categoria')    
+                             
+        
+        
+
+    def luta(self):
+            
+        if self.aprovado == True:
+            self.luta = range(0,3)
+            if self.luta == 0:
+                self.l1 = Lutador.ganharLuta()
+                self.l2 = Lutador.perderLuta()
+
+                print('********************************************')
+                print()
+                print('------------ E o VENCEDOR FOI --------------')
+                print()
+                print('********************************************')
+            
+            elif self.luta == 1:
+                self.l2 = Lutador.ganharLuta()
+                self.l1 = Lutador.perderLuta()
+
+                print('********************************************')
+                print()
+                print('------------ E o VENCEDOR FOI --------------')
+                print()
+                print('********************************************')
+
+
+            else:
+                self.l1 = Lutador.empatarLuta()
+                self.l2 = Lutador.empatarLuta()
+
+                print('********************************************')
+                print()
+                print('------------ TEMOS UM EMPATE --------------')
+                print()
+                print('********************************************')
+
+
+
+       
+
+    def get_desafiado(self):
+
+        return self.__desafiado
+
+    def set_desafiado(self,desafiado):
+
+        self.__desafiado = desafiado
+
+    def get_desafiante(self):
+
+        return self.__desafiante
+
+    def set_desafiante(self, desafiante):
+
+        self.__desafiante = desafiante
+
+    
+    def get_rounds(self):
+
+        return self.__rounds
+
+    def set_rounds(self, rounds):
+
+        self.__rounds = rounds 
+
---
+++ UFC/lutador.py
@@ -0,0 +1,116 @@
+class Lutador:
+
+    def __init__(self,nome, nascionalidade, idade, altura, peso, vitorias, derrotas, empates) -> None:
+        
+        self.__nome = nome
+        self.__nascionalidade = nascionalidade
+        self.__idade = idade
+        self.__altura = altura
+        self.__peso = peso
+        
+        self.__vitorias = vitorias
+        self.__derrotas = derrotas
+        self.__empates = empates
+
+        
+
+    def apresentar(self):
+
+       ##print('Pesando: {peso}, ele é {nacionalidade},tem {vitorias} vitorias, {derrotas} derrotas e {empates} Apresentammos o lutador {nome}')    
+         print('Pesando {}Kg ele e {} tem {} vitorias, {} derrotas e {} empates '.format(self.__peso, self.__nascionalidade, self.__vitorias, self.__derrotas, self.__empates)) 
+         print( 'Senhoras e Senhores com vcs ******** {} *******'.format(self.__nome))   
+    
+    def status(self):
+
+        print('----------------------------------------------------------')
+        print('                     CHEGOU HORA!!!!!!!             ')
+        print('----------------------------------------------------------')
+    def ganharLuta(self):
+
+        self.__vitorias = self.__vitorias + 1
+
+    def perderLuta(self):
+
+        self.__derrotas = self.__derrotas + 1
+        
+    
+    def empatarLuta(self):
+        
+        self.__empates = self.__empates + 1
+
+    def get_nome(self):
+
+        return self.__nome
+
+    def set_nome(self,nome):
+
+        self.__nome = nome    
+
+    def get_nascionalidade(self):
+
+        return self.__nacionalidade
+
+    def set_nascionalidade(self,nascionalidade):
+
+        self.__nascionalidade = nascionalidade       
+
+    def get_idade(self):
+
+        return self.__idade
+
+    def set_idade(self,idade):
+
+        self.__idade = idade    
+
+    def get_altura(self):
+
+        return self.__altura
+
+    def set_altura(self,altura):
+
+        self.__altura = altura
+
+    def get_peso(self):
+
+        return self.__peso
+
+    def set_peso(self,peso):
+
+        self.__nome = peso
+        self.set_categoria() ## por hora sem parametro
+
+    def get_categorias(self):
+
+        return self.__categoria
+
+    def set_categoria(self):
+
+      if self.__peso < 52.2:
+        self.__categoria = 'Inválido'
+      elif self.__peso <= 70.3:
+        self.__categoria = 'Leve'    
+      elif self.__peso <= 83.9:
+        self.__categoria = 'Médio'
+      elif self.__peso <= 120.2:
+        self.__categoria = 'Pesado'
+      else: self.__categoria = 'Inválido'    
+
+    def get_vitorias(self):
+
+        return self.__vitorias
+
+    def set_vitorias(self,vitorias):
+
+        self.__vitorias = vitorias
+
+    def get_empates(self):
+
+        return self.__empates
+
+    def set_empates(self,empates):
+
+        self.__empates = empates    
+        
+    
+        
+   
